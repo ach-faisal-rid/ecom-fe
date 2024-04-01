a3e0105358cf94c4f75af70dc481efbe2549719d
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-01 21:05:39 +0700

    creating necessary components for Home

---
+++ src/components/common/NavMenuMobile.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class NavMenuMobile extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NavMenuMobile
