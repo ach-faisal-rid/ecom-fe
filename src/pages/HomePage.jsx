41b86a5bc9fe847ea1c7dc44b52dd59c69b8fab6
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-02 19:48:13 +0700

    categories component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react'
+import Categories from '../components/home/Categories'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 
 class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        <Categories />
         <FeaturedProducts/>
       </Fragment>
     )
