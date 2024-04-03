e33b1c9388273c1024a6de4beef296ccc1bf2359
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-03 20:40:27 +0700

    mobile view menu part 4

--- src/components/common/NavMenuMobile.jsx
+++ src/components/common/NavMenuMobile.jsx
@@ -1,66 +1,80 @@
-import React, { useState, Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import { Navbar, Container, Row, Col, Button } from "react-bootstrap";
 import Logo from "../../assets/images/easyshop.png";
 import { Link } from "react-router-dom";
+import MegaMenuMobile from "../home/megaMenuMobile";
 
-const NavMenuMobile = () => {
-  const [sideNavState, setSideNavState] = useState("sideNavClose");
-  const [contentOverlayState, setContentOverlayState] = useState(
-    "ContentOverlayClose"
-  );
+class NavMenuMobile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      SideNavState: "sideNavClose",
+      ContentOverState: "ContentOverlayClose",
+    };
+  }
 
-  const menuBarClickHandler = () => {
-    setSideNavState(sideNavState === "sideNavOpen" ? "sideNavClose" : "sideNavOpen");
-    setContentOverlayState(
-      contentOverlayState === "ContentOverlayOpen" ? "ContentOverlayClose" : "ContentOverlayOpen"
-    );
+  MenuBarClickHandler = () => {
+    this.SideNavOpenClose();
   };
 
-  const contentOverlayClickHandler = () => {
-    setSideNavState("sideNavClose");
-    setContentOverlayState("ContentOverlayClose");
+  ContentOverlayClickHandler = () => {
+    this.SideNavOpenClose();
   };
 
-  return (
-    <Fragment>
-      <div className="TopSectionDown">
-        <Container fluid className="fixed-top shadow-sm p-2 mb-0 bg-white">
-          <Row>
-            <Col lg={4} md={4} sm={12} xs={12}>
-              <Button onClick={menuBarClickHandler} className="btn">
-                <i className="fa fa-bars"></i> Menu
-              </Button>
-              <Link to="/">
-                <img className="nav-logo" src={Logo} alt="EasyShop Logo" />
-              </Link>
-              <Button className="cart-btn">
-                <i className="fa fa-shopping-cart"></i> 3 Items
-              </Button>
-            </Col>
-          </Row>
-        </Container>
+  SideNavOpenClose = () => {
+    let SideNavState = this.state.SideNavState;
+    let ContentOverState = this.state.ContentOverState;
+    if (SideNavState === "sideNavOpen") {
+      this.setState({
+        SideNavState: "sideNavClose",
+        ContentOverState: "ContentOverlayClose",
+      });
+    } else {
+      this.setState({
+        SideNavState: "sideNavOpen",
+        ContentOverState: "ContentOverlayOpen",
+      });
+    }
+  };
 
-        <div className={sideNavState}>
-          <hr className="w-80" />
-          <div className="list-group">
-            <Link
-              to="/"
-              className="list-group-item nav-font nav-itemmenu list-group-item-action"
-            >
-              <i className="fa mr-2 fa-home"></i>
-              Home
-            </Link>
-            {/* Add more navigation links here */}
+  render() {
+    return (
+      <Fragment>
+        <div className="TopSectionDown">
+          <Container
+            fluid={"true"}
+            className="fixed-top shadow-sm p-2 mb-0 bg-white"
+          >
+            <Row>
+              <Col lg={4} md={4} sm={12} xs={12}>
+                <Button onClick={this.MenuBarClickHandler} className="btn">
+                  <i className="fa fa-bars"></i>{" "}
+                </Button>
+
+                <Link to="/">
+                  {" "}
+                  <img className="nav-logo" src={Logo} />{" "}
+                </Link>
+
+                <Button className="cart-btn">
+                  <i className="fa fa-shopping-cart"></i> 3 Items{" "}
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+
+          <div className={this.state.SideNavState}>
+            <MegaMenuMobile />
           </div>
-        </div>
 
-        <div
-          onClick={contentOverlayClickHandler}
-          className={contentOverlayState}
-        ></div>
-      </div>
-    </Fragment>
-  );
-};
+          <div
+            onClick={this.ContentOverlayClickHandler}
+            className={this.state.ContentOverState}
+          ></div>
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default NavMenuMobile;
