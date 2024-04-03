3632e56030f69a066b07fea385a9ac90c0515063
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-03 20:34:05 +0700

    mobile view menu part 2

--- src/components/common/NavMenuMobile.jsx
+++ src/components/common/NavMenuMobile.jsx
@@ -1,48 +1,66 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import { Navbar, Container, Row, Col, Button } from "react-bootstrap";
 import Logo from "../../assets/images/easyshop.png";
 import { Link } from "react-router-dom";
-class NavMenuMobile extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div className="TopSectionDown">
-          <Container
-            fluid={"true"}
-            className="fixed-top shadow-sm p-2 mb-0 bg-white"
-          >
-            <Row>
-              <Col lg={4} md={4} sm={12} xs={12}>
-                <Button className="btn">
-                  <i className="fa fa-bars"></i>{" "}
-                </Button>
 
-                <Link to="/">
-                  {" "}
-                  <img className="nav-logo" src={Logo} />{" "}
-                </Link>
+const NavMenuMobile = () => {
+  const [sideNavState, setSideNavState] = useState("sideNavClose");
+  const [contentOverlayState, setContentOverlayState] = useState(
+    "ContentOverlayClose"
+  );
 
-                <Button className="cart-btn">
-                  <i className="fa fa-shopping-cart"></i> 3 Items{" "}
-                </Button>
-              </Col>
-            </Row>
-          </Container>
-
-          <div className="sideNavOpen">
-            <hr className="w-80" />
-            <div className="list-group">
-              <a className="list-group-item nav-font nav-itemmenu list-group-item-action">
-                <i className="fa mr-2 fa-home"></i>Home{" "}
-              </a>
-            </div>
-          </div>
-
-          <div className="ContentOverlayOpen"></div>
-        </div>
-      </Fragment>
+  const menuBarClickHandler = () => {
+    setSideNavState(sideNavState === "sideNavOpen" ? "sideNavClose" : "sideNavOpen");
+    setContentOverlayState(
+      contentOverlayState === "ContentOverlayOpen" ? "ContentOverlayClose" : "ContentOverlayOpen"
     );
-  }
-}
+  };
+
+  const contentOverlayClickHandler = () => {
+    setSideNavState("sideNavClose");
+    setContentOverlayState("ContentOverlayClose");
+  };
+
+  return (
+    <Fragment>
+      <div className="TopSectionDown">
+        <Container fluid className="fixed-top shadow-sm p-2 mb-0 bg-white">
+          <Row>
+            <Col lg={4} md={4} sm={12} xs={12}>
+              <Button onClick={menuBarClickHandler} className="btn">
+                <i className="fa fa-bars"></i> Menu
+              </Button>
+              <Link to="/">
+                <img className="nav-logo" src={Logo} alt="EasyShop Logo" />
+              </Link>
+              <Button className="cart-btn">
+                <i className="fa fa-shopping-cart"></i> 3 Items
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+
+        <div className={sideNavState}>
+          <hr className="w-80" />
+          <div className="list-group">
+            <Link
+              to="/"
+              className="list-group-item nav-font nav-itemmenu list-group-item-action"
+            >
+              <i className="fa mr-2 fa-home"></i>
+              Home
+            </Link>
+            {/* Add more navigation links here */}
+          </div>
+        </div>
+
+        <div
+          onClick={contentOverlayClickHandler}
+          className={contentOverlayState}
+        ></div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default NavMenuMobile;
