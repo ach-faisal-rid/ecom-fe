8cf7471bf7bbc25b5ab7b1267ab1b44e2bd2e319
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-17 14:09:29 +0700

    perbaikan error pada tampilan homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,66 +1,60 @@
-import React, { Component, Fragment } from 'react'
-import NavMenuDesktop from '../components/common/NavMenuDesktop'
-import NavMenuMobile from '../components/common/NavMenuMobile'
-import HomeTop from '../components/home/homeTop'
-import HomeTopMobile from '../components/home/homeTopMobile'
-import Categories from '../components/home/Categories'
-import Collection from '../components/home/Collection'
-import NewArrival from '../components/home/NewArrival'
-import FeaturedProducts from '../components/home/FeaturedProducts'
-import FooterDesktop from '../components/common/FooterDesktop'
-import FooterMobile from '../components/common/FooterMobile'
-import axios from 'axios';
-import AppURL from '../api/AppURL'
+import React, { Component, Fragment } from "react";
+import NavMenuDesktop from "../components/common/NavMenuDesktop";
+import NavMenuMobile from "../components/common/NavMenuMobile";
+import HomeTop from "../components/home/homeTop";
+import HomeTopMobile from "../components/home/homeTopMobile";
+import Categories from "../components/home/Categories";
+import Collection from "../components/home/Collection";
+import NewArrival from "../components/home/NewArrival";
+import FeaturedProducts from "../components/home/FeaturedProducts";
+import FooterDesktop from "../components/common/FooterDesktop";
+import FooterMobile from "../components/common/FooterMobile";
+import axios from "axios";
+import AppURL from "../api/AppURL";
 
 class HomePage extends Component {
   componentDidMount() {
-    window.scroll(0, 0); // Gulir ke atas saat komponen dipasang
-    this.GetVisitorDetails().then(() => {
-      // Lakukan sesuatu setelah data pengunjung berhasil diambil
-    });
+    window.scroll(0, 0);
+    this.GetVisitorDetails();
   }
-  
+
   GetVisitorDetails = async () => {
     try {
-      const response = await axios.get(AppURL.VisitorDetails); // Tunggu respons
-      return response.data;
+      const response = await axios.get(AppURL.VisitorDetails);
+      // Lakukan sesuatu dengan data pengunjung yang berhasil diambil (opsional)
     } catch (error) {
-      // Tangani error
       console.error(error);
-      throw error; // Rethrow error agar dapat ditangkap di componentDidMount
+      // Tangani error dengan cara yang sesuai (misalnya, menampilkan pesan error)
     }
   };
-  
+
   render() {
     return (
       <Fragment>
         <div className="Desktop">
-        <NavMenuDesktop />
-        <HomeTop/>
-        
+          <NavMenuDesktop />
+          <HomeTop />
         </div>
-        
-        <div className='Mobile'>
-        <NavMenuMobile />  
-        <HomeTopMobile />
-        
+
+        <div className="Mobile">
+          <NavMenuMobile />
+          <HomeTopMobile />
         </div>
 
         <Categories />
-        <FeaturedProducts/>
+        <FeaturedProducts />
         <NewArrival />
         <Collection />
-        
+
         <div className="Desktop">
-        <FooterDesktop/>
+          <FooterDesktop />
         </div>
         <div className="Mobile">
-        <FooterMobile/>
+          <FooterMobile />
         </div>
-        
       </Fragment>
-    )
+    );
   }
 }
 
-export default HomePage
+export default HomePage;
