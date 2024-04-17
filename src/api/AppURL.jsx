25e7c5804c4d951933b5e0bc19105f62d0e4805e
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-17 15:28:25 +0700

    consume site info api client side part 1 bagian 1

--- src/api/AppURL.jsx
+++ src/api/AppURL.jsx
@@ -2,6 +2,7 @@ class AppURL {
  static BaseURL = "http://127.0.0.1:8000/api"
  static VisitorDetails = this.BaseURL+"/get-visitor"
  static PostContact = this.BaseURL+"/post-contact"
+ static AllSiteInfo = this.BaseURL+"/all-site-info"
 }
 
 export default AppURL
