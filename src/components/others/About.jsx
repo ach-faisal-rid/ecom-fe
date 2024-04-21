771cc8d631e18bc2aa3e13d6d28c7ee16458047c
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-21 16:05:33 +0700

    consume site info api in client side part 4 bagian 2 : meringkas kode untuk about

--- src/components/others/About.jsx
+++ src/components/others/About.jsx
@@ -1,88 +1,50 @@
-import React, { Component, Fragment } from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, h4, p } from "react-bootstrap";
 import AppURL from "../../api/AppURL";
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
 
-class About extends Component {
-  constructor() {
-    super();
-    this.state = {
-      about: "",
-      loaderDiv: "",
-      mainDiv: "d-none",
-    };
-  }
+const About = () => {
+  const [about, setAbout] = useState("");
+  const [error, setError] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get(AppURL.AllSiteInfo)
       .then((response) => {
         let StatusCode = response.status;
-        if (StatusCode == 200) {
+        if (StatusCode === 200) {
           let JsonData = response.data[0]["about"];
-          this.setState({ about: JsonData, loaderDiv: "d-none", mainDiv: "" });
+          setAbout(JsonData);
         }
       })
-      .catch((error) => {});
-  }
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+      });
+  }, []);
 
-  render() {
-    return (
-      <Fragment>
-        <Container>
-          <Row className="p-2">
-            <Col
-              className="shadow-sm bg-white mt-2"
-              md={12}
-              lg={12}
-              sm={12}
-              xs={12}
-            >
-              <div className={this.state.loaderDiv}>
-                <div class="ph-item">
-                  <div class="ph-col-12">
-                    <div class="ph-row">
-                      <div class="ph-col-4"></div>
-                      <div class="ph-col-8 empty"></div>
-                      <div class="ph-col-6"></div>
-                      <div class="ph-col-6 empty"></div>
-                      <div class="ph-col-12"></div>
-                      <div class="ph-col-12"></div>
-                      <div class="ph-col-12"></div>
-                      <div class="ph-col-12"></div>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="ph-item">
-                  <div class="ph-col-12">
-                    <div class="ph-row">
-                      <div class="ph-col-4"></div>
-                      <div class="ph-col-8 empty"></div>
-                      <div class="ph-col-6"></div>
-                      <div class="ph-col-6 empty"></div>
-                      <div class="ph-col-12"></div>
-                      <div class="ph-col-12"></div>
-                      <div class="ph-col-12"></div>
-                      <div class="ph-col-12"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className={this.state.mainDiv}>
-                <h4 className="section-title-login">About Us Page </h4>
-                <p className="section-title-contact">
-                  {ReactHtmlParser(this.state.about)}
-                </p>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Container>
+      <Row className="p-2">
+        <Col
+          className="shadow-sm bg-white mt-2"
+          md={12}
+          lg={12}
+          sm={12}
+          xs={12}
+        >
+          <div className={about ? "" : "loading"}>
+            <h4 className="section-title-login">About Us Page </h4>
+            {about && (
+              <p className="section-title-contact">{ReactHtmlParser(about)}</p>
+            )}
+            {error && <p className="text-danger">{error}</p>}
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default About;
