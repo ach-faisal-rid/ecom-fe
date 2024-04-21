f740ffa20efea73a4233cddf8ce8d4f3fd3ec7f7
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-21 16:04:24 +0700

    consume site info api in client side part 4 bagian 1 install zalog/placeholder dan mengelola tampilan about

--- src/components/others/About.jsx
+++ src/components/others/About.jsx
@@ -2,17 +2,15 @@ import React, { Component, Fragment } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import AppURL from "../../api/AppURL";
 import axios from "axios";
-import ReactHtmlParser, {
-  processNodes,
-  convertNodeToElement,
-  htmlparser2,
-} from "react-html-parser";
+import ReactHtmlParser from "react-html-parser";
 
 class About extends Component {
   constructor() {
     super();
     this.state = {
       about: "",
+      loaderDiv: "",
+      mainDiv: "d-none",
     };
   }
 
@@ -20,23 +18,16 @@ class About extends Component {
     axios
       .get(AppURL.AllSiteInfo)
       .then((response) => {
-        let statusCode = response.status; // Use lowercase for consistency
-        if (statusCode === 200) {
-          let aboutData = response.data[0].about; // Use clearer variable name
-          this.setState({ about: aboutData });
-        } else {
-          // Handle errors here (e.g., display error message)
-          console.error("Error fetching about data:", response.statusText);
+        let StatusCode = response.status;
+        if (StatusCode == 200) {
+          let JsonData = response.data[0]["about"];
+          this.setState({ about: JsonData, loaderDiv: "d-none", mainDiv: "" });
         }
       })
-      .catch((error) => {
-        // Handle network errors here (e.g., display a fallback message)
-        console.error("Error fetching about data:", error);
-      });
+      .catch((error) => {});
   }
 
   render() {
-    const { about } = this.state; // Destructuring for cleaner code
     return (
       <Fragment>
         <Container>
@@ -48,14 +39,44 @@ class About extends Component {
               sm={12}
               xs={12}
             >
-              <h4 className="section-title-login">About Us Page </h4>
-              <p className="section-title-contact">
-                {about === "" ? (
-                  <span>Fetching about information...</span>
-                ) : (
-                  ReactHtmlParser(about)
-                )}
-              </p>
+              <div className={this.state.loaderDiv}>
+                <div class="ph-item">
+                  <div class="ph-col-12">
+                    <div class="ph-row">
+                      <div class="ph-col-4"></div>
+                      <div class="ph-col-8 empty"></div>
+                      <div class="ph-col-6"></div>
+                      <div class="ph-col-6 empty"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="ph-item">
+                  <div class="ph-col-12">
+                    <div class="ph-row">
+                      <div class="ph-col-4"></div>
+                      <div class="ph-col-8 empty"></div>
+                      <div class="ph-col-6"></div>
+                      <div class="ph-col-6 empty"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className={this.state.mainDiv}>
+                <h4 className="section-title-login">About Us Page </h4>
+                <p className="section-title-contact">
+                  {ReactHtmlParser(this.state.about)}
+                </p>
+              </div>
             </Col>
           </Row>
         </Container>
