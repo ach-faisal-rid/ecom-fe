39e7870208f529e88b1a82ea49cb5501d43c9c08
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-24 19:35:28 +0700

    consume product list api part 1 bagian 2 memperbaiki tampilan card

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -23,52 +23,46 @@ class FeaturedProducts extends Component {
 
   render() {
     const FeaturedList = this.state.ProductData;
-    const MyView = FeaturedList.map((FeaturedList, i) => {
-      if (FeaturedList.special_price == "na") {
-        return (
-          <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
-            <Link to="/productdetails">
-              <Card className="image-box card">
-                <img className="center" src={FeaturedList.image} />
-                <Card.Body>
-                  <p className="product-name-on-card">{FeaturedList.title}</p>
-                  <p className="product-price-on-card">
-                    Price : ${FeaturedList.price}
-                  </p>
-                </Card.Body>
-              </Card>
-            </Link>
-          </Col>
-        );
-      } else {
-        return (
-          <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
-            <Link to="/productdetails">
-              <Card className="image-box card">
-                <img className="center" src={FeaturedList.image} />
-                <Card.Body>
-                  <p className="product-name-on-card">{FeaturedList.title}</p>
-                  <p className="product-price-on-card">
-                    Price :{" "}
-                    <strike className="text-secondary">
-                      ${FeaturedList.price}
-                    </strike>{" "}
-                    ${FeaturedList.special_price}
-                  </p>
-                </Card.Body>
-              </Card>
-            </Link>
-          </Col>
-        );
-      }
-    });
+    const MyView = FeaturedList.map((featuredProduct, i) => (
+      <Col className="p-2" key={featuredProduct.id} 
+      xl={3} 
+      lg={3} 
+      md={4} 
+      sm={4} 
+      xs={6}>
+        <Link to="/productdetails">
+          <Card className="featured-product-card shadow image-box card">
+            <img className="card-img-top img-fluid center" 
+            src={featuredProduct.image} 
+            alt={featuredProduct.title} />
+            <Card.Body>
+              <p className="product-name-on-card">
+                {featuredProduct.title}
+              </p>
+              <p className="product-price-on-card">
+                Price :{" "}
+                {featuredProduct.special_price === "na" ? (
+                  `${featuredProduct.price}`
+                ) : (
+                  <>
+                    <strike className="text-secondary">{featuredProduct.price}</strike> ${featuredProduct.special_price}
+                  </>
+                )}
+              </p>
+            </Card.Body>
+          </Card>
+        </Link>
+      </Col>
+    ));
 
     return (
       <Fragment>
         <Container className="text-center" fluid={true}>
           <div className="section-title text-center mb-55">
             <h2>FEATURED PRODUCT</h2>
-            <p>Some Of Our Exclusive Collection, You May Like</p>
+            <p>
+                Some Of Our Exclusive Collection, You May Like
+            </p>
           </div>
 
           <Row>{MyView}</Row>
