c3ddd76847dd7905e9c5e4bc159f49f2919b27ed
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-24 18:51:49 +0700

    consume productt list api part 1

--- src/api/AppURL.jsx
+++ src/api/AppURL.jsx
@@ -1,9 +1,21 @@
 class AppURL {
- static BaseURL = "http://127.0.0.1:8000/api"
- static VisitorDetails = this.BaseURL+"/get-visitor"
- static PostContact = this.BaseURL+"/post-contact"
- static AllSiteInfo = this.BaseURL+"/all-site-info"
- static AllCategoryDetails = this.BaseURL+"/all-category"
+  static BaseURL = "http://127.0.0.1:8000/api";
+  static VisitorDetails = this.BaseURL + "/get-visitor";
+  static PostContact = this.BaseURL + "/post-contact";
+  static AllSiteInfo = this.BaseURL + "/all-site-info";
+  static AllCategoryDetails = this.BaseURL + "/all-category";
+  static AllProductListByRemark(Remark) {
+    return this.BaseURL + "/all-product-list-by-remark/" 
+    + Remark;
+  }
+  static AllProductListByCategory(category) {
+    return this.BaseURL + "/all-product-list-by-category/" 
+    + category;
+  }
+  static AllProductListBysubcategory(category, subcategory) {
+    return this.BaseURL + "/all-product-list-by-subcategory/" 
+    + category+"/"+subcategory;
+  }
 }
 
-export default AppURL
+export default AppURL;
--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -1,86 +1,81 @@
-import React, { Component, Fragment } from 'react'
-import {Container,Row,Col, Card} from 'react-bootstrap'
+import React, { Component, Fragment } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import AppURL from "../../api/AppURL";
+import axios from "axios";
 
 class FeaturedProducts extends Component {
-    render() {
-            return (
-                <Fragment>
-                    <Container className="text-center" fluid={true}>
-                    
-                    <div className="section-title text-center mb-55"><h2>FEATURED PRODUCT</h2>
-                    <p>Some Of Our Exclusive Collection, You May Like</p>
-                    </div>
+  constructor() {
+    super();
+    this.state = {
+      ProductData: [],
+    };
+  }
 
+  componentDidMount() {
+    axios
+      .get(AppURL.AllProductListByRemark("FEATURED"))
+      .then((response) => {
+        this.setState({ ProductData: response.data });
+      })
+      .catch((error) => {});
+  }
 
-                    <Row>
-                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
-                        <Card className="image-box card">
-                            <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kn7sdjk0/mobile/q/j/x/c21-rmx3201-realme-original-imagfxfwbszrxkvu.jpeg?q=70" />
-                            <Card.Body> 
-                                <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                                <p className="product-price-on-card">Price : $120</p>
-                            </Card.Body>
-                        
-                        </Card>  
-                        </Col>
-                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
-                        <Card className="image-box card">
-                            <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kn7sdjk0/mobile/q/j/x/c21-rmx3201-realme-original-imagfxfwbszrxkvu.jpeg?q=70" />
-                            <Card.Body> 
-                                <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                                <p className="product-price-on-card">Price : $120</p>
-                            </Card.Body>
-                        
-                        </Card>  
-                        </Col>
-                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
-                        <Card className="image-box card">
-                            <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kn7sdjk0/mobile/q/j/x/c21-rmx3201-realme-original-imagfxfwbszrxkvu.jpeg?q=70" />
-                            <Card.Body> 
-                                <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                                <p className="product-price-on-card">Price : $120</p>
-                            </Card.Body>
-                        
-                        </Card>  
-                        </Col>
-                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
-                        <Card className="image-box card">
-                            <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kn7sdjk0/mobile/q/j/x/c21-rmx3201-realme-original-imagfxfwbszrxkvu.jpeg?q=70" />
-                            <Card.Body> 
-                                <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                                <p className="product-price-on-card">Price : $120</p>
-                            </Card.Body>
-                        
-                        </Card>  
-                        </Col>
-                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
-                        <Card className="image-box card">
-                            <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kn7sdjk0/mobile/q/j/x/c21-rmx3201-realme-original-imagfxfwbszrxkvu.jpeg?q=70" />
-                            <Card.Body> 
-                                <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                                <p className="product-price-on-card">Price : $120</p>
-                            </Card.Body>
-                        
-                        </Card>  
-                        </Col>
-                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
-                        <Card className="image-box card">
-                            <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kn7sdjk0/mobile/q/j/x/c21-rmx3201-realme-original-imagfxfwbszrxkvu.jpeg?q=70" />
-                            <Card.Body> 
-                                <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                                <p className="product-price-on-card">Price : $120</p>
-                            </Card.Body>
-                        
-                        </Card>  
-                        </Col>
-                    </Row>
+  render() {
+    const FeaturedList = this.state.ProductData;
+    const MyView = FeaturedList.map((FeaturedList, i) => {
+      if (FeaturedList.special_price == "na") {
+        return (
+          <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+            <Link to="/productdetails">
+              <Card className="image-box card">
+                <img className="center" src={FeaturedList.image} />
+                <Card.Body>
+                  <p className="product-name-on-card">{FeaturedList.title}</p>
+                  <p className="product-price-on-card">
+                    Price : ${FeaturedList.price}
+                  </p>
+                </Card.Body>
+              </Card>
+            </Link>
+          </Col>
+        );
+      } else {
+        return (
+          <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+            <Link to="/productdetails">
+              <Card className="image-box card">
+                <img className="center" src={FeaturedList.image} />
+                <Card.Body>
+                  <p className="product-name-on-card">{FeaturedList.title}</p>
+                  <p className="product-price-on-card">
+                    Price :{" "}
+                    <strike className="text-secondary">
+                      ${FeaturedList.price}
+                    </strike>{" "}
+                    ${FeaturedList.special_price}
+                  </p>
+                </Card.Body>
+              </Card>
+            </Link>
+          </Col>
+        );
+      }
+    });
 
+    return (
+      <Fragment>
+        <Container className="text-center" fluid={true}>
+          <div className="section-title text-center mb-55">
+            <h2>FEATURED PRODUCT</h2>
+            <p>Some Of Our Exclusive Collection, You May Like</p>
+          </div>
 
-                    </Container>
-
-                </Fragment>
-            )
-    }
+          <Row>{MyView}</Row>
+        </Container>
+      </Fragment>
+    );
+  }
 }
 
-export default FeaturedProducts
+export default FeaturedProducts;
